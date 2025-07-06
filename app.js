ecec3039dc9dd8740d5de09d4eee737e5b4f1ffb
Torin <416347856@qq.com>
2025-07-06 16:14:45 +0800

    [feature] remove the random feature.

--- app.js
+++ app.js
@@ -14,14 +14,6 @@ $(document).ready(function(){
         });
     })
 
-    // Fetches random recipe
-    $('.btnRandomRecipe').on('click', function(){
-        fetchMeal('r');
-
-        // Textual updates
-        $('#dynamicTitle').text('The Random Recipe');
-    });
-
     // Fetch searched recipe
     $('.btnSearchRecipe').on('click', function(){
         fetchMeal('u');
@@ -30,7 +22,7 @@ $(document).ready(function(){
     //also this could be easily refactored, maybe open issue for this too
 
     // Fetch content after 3s
-    setTimeout(getData(['u', 'r']), 1000);
+    setTimeout(getData(['u']), 1000);
 });
 
 // Get recipe list based on search input
@@ -43,22 +35,13 @@ $(document).keypress(function(e) {
 // Show recipe of clicked meal
 $(document).on('click','.mealCardRecipeBtn',function(){
     let meal = $(this).data('meal');
-    if(meal.strCategory === undefined){
-        fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i='+meal.idMeal)
-        .then( res => res.json() )
-        .then( res => {
-            meal = res.meals[0];
-            window.scrollTo(0,$('#random').offset().top);
-            createMeal(meal,'r');
-            // Textual updates
-            $('#dynamicTitle').text(meal.strMeal);
-        })
-    } else {
-        window.scrollTo(0,$('#random').offset().top);
-        createMeal(meal,'r');
-        // Textual updates
-        $('#dynamicTitle').text(meal.strMeal);
-    }
+    fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i='+meal.idMeal)
+    .then( res => res.json() )
+    .then( res => {
+        meal = res.meals[0];
+        // 此处可添加新的食谱展示逻辑
+    })
+    .catch(e => console.warn(e));
 });
 
 // Clear search box on button press
@@ -68,18 +51,7 @@ $(document).on('click','.clear-field',function(){
 
 // Uses the fetch() API to request random meal recipe from TheMealsDB.com API
 function fetchMeal(type){
-    let url = '';
-    if ( type === 'r') { url = 'https://www.themealdb.com/api/json/v1/1/random.php'; }
-
-    if ( type === 'r' ) {
-        fetch(url)
-        .then( res => res.json() )
-        .then( res => {
-            createMeal(res.meals[0], type);
-            setCache(res.meals[0], type);
-        })
-        .catch( e => console.warn(e) );
-    } else {
+    if ( type === 'u' ) {
         fetch('https://www.themealdb.com/api/json/v1/1/search.php?s='+$.trim($('#searchRecipe').val()))
         .then( res => res.json() )
         .then( res => {
@@ -107,30 +79,8 @@ $(document).on('click','#errorMessageBtn',function(){
 // Function to save the data in the cache
 const setCache = (meal, type) => {
     let mealJson = JSON.stringify(meal);
-    if( type === 'u' ){
-        sessionStorage.setItem("search", $.trim($('#searchRecipe').val()));
-        sessionStorage.setItem(type, mealJson);
-    } else setCookie(type, mealJson);
-
-}
-
-// Function to set the cookie
-const setCookie = (key, value, exDays = 3) => {
-    let date = new Date();
-    date.setTime(date.getTime() + exDays*24*60*60*1000);
-    document.cookie = key + "=" + value + "; expires=" + date.toUTCString() + ";path=/";
-}
-
-// Function to get cookie
-const getCookie = (key) => {
-    key = key + "=";
-    var cookies = document.cookie.split(';');
-    for(var i = 0; i < cookies.length; i++) {
-      var cookie = cookies[i];
-      while (cookie.charAt(0) == ' ') cookie = cookie.substring(1);
-      if (cookie.indexOf(key) == 0) { return cookie.substring(key.length, cookie.length) };
-    }
-    return null;
+    sessionStorage.setItem("search", $.trim($('#searchRecipe').val()));
+    sessionStorage.setItem(type, mealJson);
 }
 
 // Function to get cache data if it exists, otherwise, fetch from the API
@@ -144,13 +94,6 @@ const getData = (types) => {
                 $('#userInput').text(sessionStorage.getItem("search"));
             }
         }
-        else {
-            let mealData = null;
-            try {
-                mealData = JSON.parse(getCookie(type));
-            } catch (error) { console.warn(error) };
-            mealData !== null ? createMeal(mealData, type) : fetchMeal(type);
-        }
     })
 }
 
@@ -163,80 +106,6 @@ function fetchCategoryMeal(category){
         })
     .catch(e => console.warn(e));
     $('#userInput').text(category);
-}
-
-// Function to generate the random meal UI component
-const createMeal = (meal,type) => {
-    // Set meal thumbnail
-    setMealThumbnail(meal,type);
-
-    let mealMetadata = '', mealInstr = '';
-
-    // Fill meal name 
-    if ( meal.strMeal ) { 
-        mealMetadata = `<span>Name:</span> ${meal.strMeal} <br/>`
-    }
-
-    // Fill Area 
-    if ( meal.strArea ) {
-        mealMetadata += `<span>Area:</span> ${meal.strArea} <br/>`
-    }
-
-    // Fill category 
-    if ( meal.strCategory ) {
-        mealMetadata += `<span>Category:</span> ${meal.strCategory} <br/>`
-    }
-
-    // Format tags with comma-whitespace separator
-    if ( meal.strTags ) {
-        mealMetadata += `<span>Tags:</span> ${meal.strTags.split(',').join(', ')} <br/>`
-    }
-
-    // Set YouTube link
-    if ( meal.strYoutube ) {
-        mealMetadata +=`<span>YouTube:</span> <a href='${meal.strYoutube}' target="_blank" title="Watch how to cook ${meal.strMeal}">${meal.strYoutube}</a><br/>`
-    }
-
-     // Set Source link
-     if ( meal.strSource ) {
-        mealMetadata +=`<span>Source:</span> <a href='${meal.strSource}' target="_blank" title="Watch how to cook ${meal.strMeal}">${meal.strSource}</a><br/>`
-    }
-
-    // Fill ingredients
-    let ingredients = [];
-    setIngredients(meal, ingredients);
-    if ( ingredients.length > 0 ) {
-        mealMetadata +=`<span>Ingredients:</span> <br/> <ul>${ingredients.join('')}</ul>`
-    }
-
-    // Set instructions
-    if ( meal.strInstructions ) {
-        mealInstr =`<span>Instructions:</span> <br/> ${meal.strInstructions}`
-    }
-    
-    if ( type === 'r') { 
-        $('#randomMealMetadata').html(mealMetadata); 
-        $('#randomMealInstructions').html(mealInstr); 
-    }
-}
-
-// Sets random meal's thumbnail image
-const setMealThumbnail = (meal,type) => {
-    let imgSrc = `<img src="${meal.strMealThumb}" alt="${meal.strMeal}" title="${meal.strMeal}" />`;
-    if ( type === 'r') { $('#randomMealImg').html(imgSrc); }
-}
-
-// Gets ingredients of the random meal
-const setIngredients = (meal,ingredients) => {   
-    // API returns max. 20 ingredients
-    for(let i = 1; i <= 20; i++){
-        if(meal[`strIngredient${i}`]){
-            ingredients.push(
-                `<li>${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}</li>`
-            );
-        } else { break; }
-        if ( i % 2 === 0 ) { ingredients.push('<br/>'); }
-    }
 }
 
 // Creates meal cards based on search form
